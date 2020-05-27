b77fac3a78a560e515db60d524ec9e9ed14db655
Aditya Patadia <aditya@patadia.org>
2020-05-27 18:01:02 +0530

    brizy builder fixes

--- includes/class-gumlet.php
+++ includes/class-gumlet.php
@@ -115,6 +115,7 @@ class Gumlet
              || (isset($_GET['PageSpeed']) && $_GET['PageSpeed'] == 'off') || strpos($referrerPath, 'PageSpeed=off')
              ||  isset($_GET['fl_builder']) || strpos($referrerPath, '/?fl_builder') //sssh.... Beaver Builder is editing :)
              ||  isset($_GET['brizy-edit']) || strpos($referrerPath, '/?brizy-edit') //sssh.... Brizy Builder is editing :)
+             ||  isset($_GET['brizy-edit-iframe']) || strpos($referrerPath, '/?brizy-edit-iframe') //sssh.... Brizy Builder is editing :)
              || (isset($_GET['tve']) && $_GET['tve'] == 'true') //Thrive Architect editor (thrive-visual-editor/thrive-visual-editor.php)
              || (isset($_GET['ct_builder']) && $_GET['ct_builder'] == 'true') //Oxygen Builder
              || (isset($_GET['action']) && $_GET['action'] == 'ct_render_shortcode') // oxygen templates
