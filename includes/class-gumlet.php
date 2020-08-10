6ce327b167024b18a4ea337785bf483386a07f4e
Aditya Patadia <aditya@patadia.org>
2020-08-10 13:37:15 +0530

    fix strpos detection

--- includes/class-gumlet.php
+++ includes/class-gumlet.php
@@ -109,7 +109,7 @@ class Gumlet
         $referrerPath = (isset($referrer['path']) ? $referrer['path'] : '');
         return !(
             is_feed()
-             || strpos($_SERVER['REQUEST_URI'], "/feed/") != false
+             || strpos($_SERVER['REQUEST_URI'], "/feed/") !== false
              || (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
              || (defined('DOING_CRON') && DOING_CRON)
              || (defined('WP_CLI') && WP_CLI)
